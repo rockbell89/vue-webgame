43177aa3e3bbde99887408f438b9d0b5ac4c6067
BORA KIM <rockbell89@gmail.com>
2019-10-08 23:44:56 +0900

    vuejs study web-game

---
+++ 3.숫자야구/components/NumberBaseball.vue
@@ -0,0 +1,90 @@
+<template lang="html">
+  <div>
+    <h1>{{result}}</h1>
+    <form action="" @submit.prevent="onSubmitForm">
+      <input type="number" ref="answer" minlength="4
+      " maxlength="4" v-model="value">
+      <button type="type">입력</button>
+    </form>
+    <p>정답: {{answer}}</p>
+    <p>시도: {{tries.length}}</p>
+    <ul>
+      <li v-for="item in tries">
+        <p>{{item}}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+const getNumbers = () => {
+  const candidates = [1,2,3,4,5,6,7,8,9];
+  const array = [];
+  for(let i = 0; i < 4; i++){
+    const chosen = candidates.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+    console.log(chosen);
+    array.push(chosen);
+  }
+  return array;
+};
+
+export default {
+  data() {
+    return {
+      answer: getNumbers(),
+      tries: [],
+      value: '',
+      result: ''
+    }
+  },
+  methods: {
+    onSubmitForm(e) {
+      console.log(this.answer.join(''), this.value);
+      if(this.answer.join('') === this.value){
+        this.tries.push({
+          try: this.value,
+          result: '홈런'
+        });
+        this.value ='';
+        this.$refs.answer.focus();
+        this.answer = getNumbers();
+      }else{
+        if(this.tries.length >= 9){
+          this.result = '패배 !';
+          alert('게임을 다시 시작하세요.');
+          this.answer = getNumbers();
+        }else{
+          var strikes =0;
+          var ball = 0;
+          var answerArray = this.value.split('').map(v => parseInt(v));
+          console.log('answerArray: ' , answerArray);
+          for(let i =0; i <4; i++){
+            if(this.answer[i] === answerArray[i]){
+              // strikes 숫자 + 자릿수
+              strikes++;
+            }else if(this.answer.includes(answerArray[i])){
+              // ball 숫자
+              ball ++;
+            }
+          }
+          this.tries.push({
+            try: this.value,
+            result: `${strikes} 스트라이크 ${ball} 볼 입니다.`
+          });
+        }
+        this.value ='';
+        this.$refs.answer.focus();
+
+
+
+      }
+
+
+      this.$refs.answer.focus();
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ 3.숫자야구/main.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'; //vue cdn 을 vue cli 설치후 import
+import NumberBaseball from './components/NumberBaseball.vue';
+
+new Vue(NumberBaseball).$mount('#app'); // el
